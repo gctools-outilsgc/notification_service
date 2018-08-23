94959efe5514660b8aa4fbfe2825c7614d65ab38
Bryan Robitaille <bryan.robitaille@tbs-sct.gc.ca>
2018-08-23 13:51:08 -0400

    Fix ability to handle null on notifications query

--- src/resolvers/Query.js
+++ src/resolvers/Query.js
@@ -1,12 +1,12 @@
 function notifications(_, args, context, info){
+    whereData={where:{}}
+    whereData.where={for: args.for}
+    if (args.viewed !== undefined){
+        whereData.where.push({online:{viewed:args.viewed}})
+    }
     return context.prisma.query.notifications(
         {
-            where:{
-                for: args.for,
-                online: {
-                    viewed: args.viewed
-                }
-            },
+            whereData,
             skip: args.skip,
             first: args.first,
         },
